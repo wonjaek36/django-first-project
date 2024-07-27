e2bcac68b873359ef2e04ec71c362f9d6b2b7867
Wonjae Kim <wonjaek36@gmail.com>
2024-07-27 09:44:40 +0900

    Django Tutorial: Part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,26 +2,32 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list
-    }
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(generic.ListView):
+    # Default template_name  is <app_name>/<model_name>_list.html
+    template_name = "polls/index.html"
+    # Override context name from default question_list to latest_question_list
+    context_object_name = "latest_question_list"
     
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]    
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+
+class DetailView(generic.DetailView):
+    # Default template_name is <app_name>/<model_name>_detail.html
+    model = Question
+    # context name is question (default)
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
